086bf4dbd94d12d0e0d3e5d7d8654cae7969ccc1
Lucas <howaitosame13@gmail.com>
2024-09-17 17:26:27 -0300

    created page events and register

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let indice = 0;
 let elemento = document.getElementById("dynamic-text");
 let isDeleting = false;
 let charIndex = 0;
-let velocidade = 100; // Velocidade da digitação
+let velocidade = 100; 
 
 function digitar() {
   const textoAtual = textos[indice];
@@ -11,27 +11,26 @@ function digitar() {
   if (!isDeleting && charIndex < textoAtual.length) {
     elemento.textContent += textoAtual.charAt(charIndex);
     charIndex++;
-    setTimeout(digitar, velocidade); // Adiciona letra por letra
+    setTimeout(digitar, velocidade);
   } else if (isDeleting && charIndex > 0) {
     elemento.textContent = textoAtual.substring(0, charIndex - 1);
     charIndex--;
-    setTimeout(digitar, velocidade); // Remove letra por letra
+    setTimeout(digitar, velocidade);
   } else if (!isDeleting && charIndex === textoAtual.length) {
     isDeleting = true;
-    setTimeout(digitar, 1000); // Pausa antes de apagar o texto
+    setTimeout(digitar, 1000); 
   } else if (isDeleting && charIndex === 0) {
     isDeleting = false;
-    indice = (indice + 1) % textos.length; // Vai para o próximo texto
-    setTimeout(digitar, 500); // Pausa antes de começar a digitar o próximo texto
+    indice = (indice + 1) % textos.length;
+    setTimeout(digitar, 500); 
   }
 }
 
 document.querySelector('.arrow-link').addEventListener('click', function(e) {
-  e.preventDefault();  // Evita o comportamento padrão de pular diretamente
+  e.preventDefault();
   document.querySelector('#about-us').scrollIntoView({
-      behavior: 'smooth'  // Comportamento de rolagem suave
+      behavior: 'smooth' 
   });
 });
 
-// Começa a digitar imediatamente
 digitar();
